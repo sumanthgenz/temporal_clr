c4964da6a050ae447d7bb1b491d9c1cb14f5e4b3
Sumanth G <sumanthgenz@gmail.com>
2020-12-28 15:44:23 -0800

    Update augment.py

--- augment.py
+++ augment.py
@@ -15,7 +15,7 @@ import warnings
 import glob
 
 from metrics import *
-from torchaudio_transforms import *
+from transforms import *
 
 torchaudio.set_audio_backend("sox_io") 
 os.environ["IMAGEIO_FFMPEG_EXE"] = "/home/sgurram/anaconda3/bin/ffmpeg"
@@ -55,13 +55,14 @@ def augment(sample, wave_transform, spec_transform, threshold, fixed_crop=True):
     #     return spec_transform(threshold)(SpecFixedCrop(threshold)(spec[15:]))
 
 
-    #cropping mel-bins by [15:] to remove NaNs
     if fixed_crop:
-        return spec_transform(threshold)(SpecFixedCrop(threshold)(spec[15:]))
-
-    return spec_transform(threshold)(SpecRandomCrop(threshold)(spec[15:]))
-
-    # return SpecCrop(threshold)(spec)
+        spec = spec_transform(threshold)(SpecFixedCrop(threshold)(spec))
+        spec[torch.isinf(spec)] = 0
+        return spec
+    
+    spec = spec_transform(threshold)(SpecRandomCrop(threshold)(spec))
+    spec[torch.isinf(spec)] = 0
+    return spec
 
 def get_augmented_views(path):
     sample, _ = get_wave(path)
@@ -85,7 +86,7 @@ def get_augmented_views(path):
 
     return augment(sample, wave1, spec1, threshold1), augment(sample, wave2, spec2, threshold2), (wave1, spec1), (wave2, spec2)
 
-def get_temporal_shuffle_views(path):
+def get_temporal_permutes(path):
     sample, _ = get_wave(path)
     wave = WaveIdentity
     spec1 = SpecIdentity
@@ -93,20 +94,31 @@ def get_temporal_shuffle_views(path):
     threshold = random.uniform(0.0, 0.5)
 
     # Return (anchor, permutes), anchor is single sample, permutes is a list of samples
-    return augment(sample, wave, spec1, threshold fixed_crop=False), augment(sample, wave, spec2, threshold1)
+    return augment(sample, wave, spec1, threshold, fixed_crop=False), augment(sample, wave, spec2, threshold)
     
+def get_temporal_shuffle(path):
+    #assume num_segments = 4
+    shuffle_idx = random.randint(0, 23)
+    anchor, permutes = get_temporal_permutes(path)
+    # return permutes[shuffle_idx], shuffle_idx
+    return permutes[shuffle_idx], shuffle_idx
+
+
 if __name__ == '__main__':
     for _ in tqdm(range(1)):
         filepath = "/{dir}/kinetics_audio/train/25_riding a bike/0->--JMdI8PKvsc.wav".format(dir = data)
         # view1, view2, _, _ = get_augmented_views(filepath)
-        permutes = get_temporal_shuffle_views(filepath)
-        view1, view2 = permutes[5], permutes[10]
+        shuffle, label = get_temporal_shuffle(filepath)
+ 
+    #     view1, view2 = permutes[5], permutes[10]
     
-    print(permutes.shape)
-    f = plt.figure()
-    f.add_subplot(1, 2, 1)
-    plt.imshow(view1)
+    # print(permutes.shape)
+    # f = plt.figure()
+    # f.add_subplot(1, 2, 1)
+    # plt.imshow(view1)
 
-    f.add_subplot(1, 2, 2)
-    plt.imshow(view2)
-    plt.savefig("Desktop/log_mel_two_views.png")
+    # f.add_subplot(1, 2, 2)
+    # plt.imshow(view2)
+    # plt.savefig("Desktop/log_mel_two_views.png")
+
+
